a252d8d3815ea34c0461017447ef8f44438a33b2
muriel durand <you@example.com>
2019-03-13 16:36:33 +0100

    Post

--- routes/ville.js
+++ routes/ville.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/ville', function(req, res, next) {
-  res.render('ville', { title: req.query.nom_ville });
+router.post('/ville', function(req, res, next) {
+  res.render('ville', { title: req.body.nom_ville });
+//req.querry.nom_ville
 console.log("Page ville");
 });
 
